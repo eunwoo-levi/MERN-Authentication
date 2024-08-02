893056446a5cb16931a5e0f825a645105ce3531f
eunwoo-levi <eunwoo1341@gmail.com>
2024-08-02 13:45:09 +0900

    feat:NextAuth update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,43 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
 import { SessionStrategy } from "next-auth";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from "bcryptjs"
 
 const authOptions = {
     providers:[
         CredentialsProvider({
             name: "credentials",
-            credentials:{},
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
 
-            async authorize(credentials){
-                const user = {id:"1"};
-                return user
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    return null;
+                }
+                const {email, password} = credentials
+
+                try{
+                    await connectMongoDB()
+                    const user = await User.findOne({email})
+
+                    if(!user){
+                        return null
+                    }
+
+                    const passwordsMatch = await bcrypt.compare(password, user.password)
+                    if(!passwordsMatch){
+                        return null
+                    }
+
+                    return user
+
+                }catch(error){
+                    console.log("Error: ", error)
+                }
             }
         })
     ],
--- components/Userinfo.tsx
+++ components/Userinfo.tsx
@@ -1,14 +1,23 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+
 export default function Userinfo() {
+  const { data: session } = useSession();
+
   return (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-8 bg-zinc-300/10 flex flex-col gap-2 my-6">
         <div>
-          Name: <span className="font-bold">John</span>
+          Name: <span className="font-bold">{session?.user?.name}</span>
         </div>
         <div>
-          Email: <span className="font-bold">John@gmail.com</span>
+          Email: <span className="font-bold">{session?.user?.email}</span>
         </div>
-        <button className="bg-red-500 text-white font-bold px-6 py-2 mt-3">
+        <button
+          onClick={() => signOut()}
+          className="bg-red-500 text-white font-bold px-6 py-2 mt-3"
+        >
           Log Out
         </button>
       </div>
